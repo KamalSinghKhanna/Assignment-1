1aab87fa402ebdb6c48709dcec9037ff33b4f47e
KamalSinghKhanna <kamalsinghkhanna2706@gmail.com>
2022-07-07 11:33:39 +0530

    done

---
+++ src/Firebase.js
@@ -0,0 +1,39 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFaX_G1EmYl5fK5pbbP2jKGEdxhz94MTs",
+  authDomain: "auth-d4bf3.firebaseapp.com",
+  projectId: "auth-d4bf3",
+  storageBucket: "auth-d4bf3.appspot.com",
+  messagingSenderId: "307779910137",
+  appId: "1:307779910137:web:5465ca9a613a0748786528"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+
+const provider = new GoogleAuthProvider();
+export var isLoggedIn = false;
+
+export const signInWithGoogle = () => {
+    
+    signInWithPopup(auth, provider).then((result) => {
+        
+        const name = result.user.displayName
+        const email = result.user.email
+        const profilePic = result.user.photoURL
+
+        localStorage.setItem('name', name)
+        localStorage.setItem('email', email)
+        localStorage.setItem('profilePic', profilePic )
+    }).catch((error) =>{
+        console.log(error)
+    })
+
+    isLoggedIn = true;
+}
---
+++ src/component/Timer.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import "./timer.css";
+
+function Timer() {
+const [start, setStart] = useState(false);
+
+
+  const [seconds, setSeconds] = useState(0);
+
+
+  const [minutes, setMinutes] = useState(25);
+  const [displayMsg, setDisplayMsg] = useState(false);
+  
+  
+  let interval;
+  useEffect(() => {
+    if (start) {
+      interval = setInterval(() => {
+        if (seconds === 0) {
+          if (minutes !== 0) {
+            setSeconds(59);
+            let newMinutes = minutes -1;
+            setMinutes(newMinutes);
+          } else {
+            let minutes = displayMsg ? 24 : 4;
+            let seconds = 59;
+            setMinutes(minutes);
+            setSeconds(seconds);
+            setDisplayMsg(!displayMsg);
+          }
+        } else {
+            let newSeconds = seconds -1;
+          setSeconds(newSeconds);
+        }
+      }, 1000);
+    }
+  }, [start, seconds]);
+
+  const reStart = () => {
+    setSeconds(0);
+    setMinutes(25);
+  };
+
+  const stop = () => {
+    clearInterval(interval);
+  };
+
+  const toggleStart = () => {
+    setStart(!start);
+  };
+  const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  return (
+    <div className="timer">
+      <div className="box">
+        {displayMsg && <h3 className="second-timer">5 minutes Timer</h3>}
+        <div className="timer-box">
+          <h1>Timer</h1>
+          <h1>
+            {timerMinutes}:{timerSeconds}
+          </h1>
+
+          <button
+            className={!start ? "btn start" : "btn stop"}
+            onClick={toggleStart}
+          >
+            {!start ? "START" : "STOP"}
+          </button>
+          <button className="btn  restart" onClick={reStart}>
+            restart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Timer;
